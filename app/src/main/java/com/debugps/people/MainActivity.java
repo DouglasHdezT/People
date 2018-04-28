03ca092f8b4a7c152ca8f13940f06b50c126fe48
Douglas_HdezT <00004516@uca.edu.sv>
2018-04-28 13:13:08 -0600

    Vista en portrait y landscape

--- app/src/main/java/com/debugps/people/MainActivity.java
+++ app/src/main/java/com/debugps/people/MainActivity.java
@@ -1,9 +1,13 @@
 package com.debugps.people;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
+
+
     /*
-    Inflando el boton de busqueda en la ActionBar
+    Inflando el boton de busqueda en la ActionBar...
      */
 
     @Override
@@ -22,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView s = (SearchView) menu.findItem(R.id.search_main).getActionView();
+
+        //Listener del search bar...
+
+        s.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(getApplicationContext(),newText,Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
         return true;
     }
 }
+
