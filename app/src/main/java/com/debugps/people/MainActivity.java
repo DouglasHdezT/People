8ac0bb5cb06475d7ebb3c41dfe358c6bd96f114b
Douglas_HdezT <00004516@uca.edu.sv>
2018-04-28 14:37:47 -0600

    Clase de control de datos Contact

--- app/src/main/java/com/debugps/people/MainActivity.java
+++ app/src/main/java/com/debugps/people/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
 
 
---
+++ app/src/main/java/com/debugps/people/data/Contact.java
@@ -0,0 +1,83 @@
+package com.debugps.people.data;
+
+import android.graphics.Bitmap;
+
+public class Contact {
+
+    private String name;
+    private String phoneNumbers;
+    private String email;
+    private String address;
+    private String birthday;
+    private boolean favorite;
+    private Bitmap profileImage;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phoneNumbers, String email, String address, String birthday, boolean favorite, Bitmap profileImage) {
+        this.name = name;
+        this.phoneNumbers = phoneNumbers;
+        this.email = email;
+        this.address = address;
+        this.birthday = birthday;
+        this.favorite = favorite;
+        this.profileImage = profileImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(String phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public Bitmap getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(Bitmap profileImage) {
+        this.profileImage = profileImage;
+    }
+}
