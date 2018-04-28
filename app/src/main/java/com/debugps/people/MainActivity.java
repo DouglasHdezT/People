6881a6b29b21915a3671af0770113babacf5bbbf
Douglas_HdezT <00004516@uca.edu.sv>
2018-04-27 18:06:57 -0600

    Creacioon del proyecto e instacia de la barra de busqueda

---
+++ app/src/main/java/com/debugps/people/MainActivity.java
@@ -0,0 +1,27 @@
+package com.debugps.people;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    Inflando el boton de busqueda en la ActionBar
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+}
